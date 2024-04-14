4b1cbd328cff523f264e9cffee3203684e616526
Sebastien Aucante <sebastien.aucante@gmail.com>
2024-04-14 22:21:04 +0200

    Entity: Create Configuration

---
+++ src/main/java/com/example/neofinanceapi/models/Configuration.java
@@ -0,0 +1,23 @@
+package com.example.neofinanceapi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "\"configuration\"")
+public class Configuration {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String currency;
+
+    private Boolean isLightMode;
+}
