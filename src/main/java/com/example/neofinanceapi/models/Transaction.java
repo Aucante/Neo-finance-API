99d1bfc5ca9742297e0c7bedb54e283b14e6cdbf
Sebastien Aucante <sebastien.aucante@gmail.com>
2024-04-14 22:28:37 +0200

    Entity: Create Transaction

---
+++ src/main/java/com/example/neofinanceapi/models/Transaction.java
@@ -0,0 +1,33 @@
+package com.example.neofinanceapi.models;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Entity
+@Table(name = "\"transaction\"")
+public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private Float price;
+
+    private Long quantity;
+
+    private LocalDateTime date;
+
+    private Float fees;
+
+    private String note;
+
+    private Boolean isPurchased;
+}
