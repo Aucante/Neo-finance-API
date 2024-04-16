02f494f493ed41895b32466e1f29c5999c5cf07c
Sebastien Aucante <sebastien.aucante@gmail.com>
2024-04-16 22:13:47 +0200

    DTO: Configuration

---
+++ src/main/java/com/example/neofinanceapi/dto/ConfigurationDto.java
@@ -0,0 +1,39 @@
+package com.example.neofinanceapi.dto;
+
+import com.example.neofinanceapi.models.Configuration;
+import com.example.neofinanceapi.models.User;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+public class ConfigurationDto {
+
+    private String currency;
+
+    private Boolean isLightMode;
+
+    private UserDto userDto;
+
+    public static ConfigurationDto fromConfigurationEntity(Configuration configuration) {
+        return ConfigurationDto.builder()
+                .currency(configuration.getCurrency())
+                .isLightMode(configuration.getIsLightMode())
+                .userDto(UserDto.fromUserEntity(configuration.getUser()))
+                .build();
+    }
+
+    public static Configuration toConfigurationEntity(ConfigurationDto configurationDto) {
+        return Configuration.builder()
+                .currency(configurationDto.getCurrency())
+                .isLightMode(configurationDto.getIsLightMode())
+                .user(UserDto.toUserEntity(configurationDto.getUserDto()))
+                .build();
+    }
+}
