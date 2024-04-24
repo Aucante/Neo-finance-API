41f502a8d4138cad65a05cf0de643135e35c01ba
Sebastien Aucante <sebastien.aucante@gmail.com>
2024-04-24 22:17:43 +0200

    Validator & exceptions handlers

--- src/main/java/com/example/neofinanceapi/validators/ObjectValidationException.java
+++ src/main/java/com/example/neofinanceapi/validators/ObjectValidationException.java
@@ -1,9 +1,11 @@
 package com.example.neofinanceapi.validators;
 
+import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 
 import java.util.Set;
 
+@Getter
 @RequiredArgsConstructor
 public class ObjectValidationException extends RuntimeException {
 
