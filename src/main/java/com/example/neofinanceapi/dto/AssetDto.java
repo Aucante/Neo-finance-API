170987b150c690aef695da6d8969b101b57d3f44
Sebastien Aucante <sebastien.aucante@gmail.com>
2024-04-25 21:27:08 +0200

    Validation constraints: Asset

--- src/main/java/com/example/neofinanceapi/dto/AssetDto.java
+++ src/main/java/com/example/neofinanceapi/dto/AssetDto.java
@@ -1,6 +1,9 @@
 package com.example.neofinanceapi.dto;
 
 import com.example.neofinanceapi.models.Asset;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -14,12 +17,21 @@ import java.math.BigDecimal;
 @AllArgsConstructor
 public class AssetDto {
 
+    private static final int NAME_MIN_SIZE = 3;
+    private static final int NAME_MAX_SIZE = 15;
+    private static final int ABBREVIATION_SIZE = 3;
+
     private Integer id;
 
+    @NotBlank(message = "The asset name must be filled.")
+    @Size(min = NAME_MIN_SIZE, max = NAME_MAX_SIZE, message = "The asset name must be between " + NAME_MIN_SIZE + " and " + NAME_MAX_SIZE + " characters.")
     private String name;
 
+    @NotBlank(message = "Asset abbreviation must be filled.")
+    @Size(min = ABBREVIATION_SIZE, max = ABBREVIATION_SIZE, message = "The asset abbreviation must have exactly " + ABBREVIATION_SIZE + " characters.")
     private String abbreviation;
 
+    @Positive(message = "The value must be positive.")
     private BigDecimal value;
 
     public static AssetDto fromAssetEntity(Asset asset) {
--- src/main/java/com/example/neofinanceapi/services/Impl/AssetServiceImpl.java
+++ src/main/java/com/example/neofinanceapi/services/Impl/AssetServiceImpl.java
@@ -1,6 +1,7 @@
 package com.example.neofinanceapi.services.Impl;
 
 import com.example.neofinanceapi.dto.AssetDto;
+import com.example.neofinanceapi.exceptions.ObjectsValidator;
 import com.example.neofinanceapi.models.Asset;
 import com.example.neofinanceapi.repositories.AssetRepository;
 import com.example.neofinanceapi.services.AssetService;
@@ -17,8 +18,11 @@ public class AssetServiceImpl implements AssetService {
 
     private final AssetRepository assetRepository;
 
+    private final ObjectsValidator<AssetDto> validator;
+
     @Override
     public Integer save(AssetDto dto) {
+        validator.validate(dto);
         Asset asset = AssetDto.toAssetEntity(dto);
 
         return assetRepository.save(asset).getId();
