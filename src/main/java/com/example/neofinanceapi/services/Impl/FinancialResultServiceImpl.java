31046242eea26921ee2ec5167d73647466dd9d0f
Sebastien Aucante <sebastien.aucante@gmail.com>
2024-04-27 17:15:16 +0200

    Validation constraints: Financial result

--- src/main/java/com/example/neofinanceapi/services/Impl/FinancialResultServiceImpl.java
+++ src/main/java/com/example/neofinanceapi/services/Impl/FinancialResultServiceImpl.java
@@ -1,10 +1,8 @@
 package com.example.neofinanceapi.services.Impl;
 
 import com.example.neofinanceapi.dto.FinancialResultDto;
-import com.example.neofinanceapi.dto.UserDto;
 import com.example.neofinanceapi.exceptions.ObjectsValidator;
 import com.example.neofinanceapi.models.FinancialResult;
-import com.example.neofinanceapi.models.User;
 import com.example.neofinanceapi.repositories.FinancialResultRepository;
 import com.example.neofinanceapi.services.FinancialResultService;
 import jakarta.persistence.EntityNotFoundException;
