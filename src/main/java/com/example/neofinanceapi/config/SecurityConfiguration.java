0b77d9f113f18d6f28ff67d4f555b4ce89697503
Sebastien Aucante <sebastien.aucante@gmail.com>
2024-08-25 17:53:45 +0200

    CORS front local url authorization

--- src/main/java/com/example/neofinanceapi/config/SecurityConfiguration.java
+++ src/main/java/com/example/neofinanceapi/config/SecurityConfiguration.java
@@ -67,7 +67,10 @@ public class SecurityConfiguration {
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:8005"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:8005",
+                "http://localhost:4200"
+        ));
         configuration.setAllowedMethods(List.of("GET","POST"));
         configuration.setAllowedHeaders(List.of("Authorization","Content-Type"));
 
